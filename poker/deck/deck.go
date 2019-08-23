80679fef53ea3b65fff4217b7d800a0d4f6b3d0a
imran31415 <imran@vsco.co>
2019-08-22 22:53:40 -0700

    integrate evaluation of hands with deck of cards

--- poker/deck/deck.go
+++ poker/deck/deck.go
@@ -1,6 +1,76 @@
 package deck
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Seed our randomness with the current time
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// shamelessly stolen from https://gist.github.com/montanaflynn/4cc2779d2e353d7524a7bdce57869a75
+// Card holds the card suits and types in the deck
 type Card struct {
-	suite string
-	rank  string
+	Type string
+	Suit string
+}
+
+func (c *Card) String() string {
+	return fmt.Sprintf("%s%s", c.Type, c.Suit)
+}
+
+type Deck []Card
+
+func New() (deck Deck) {
+	types := []string{"2", "3", "4", "5", "6", "7",
+		"8", "9", "T", "J", "Q", "K", "A"}
+
+	// Valid suits include Heart, Diamond, Club & Spade
+	suits := []string{"h", "d", "c", "s"}
+
+	// Loop over each type and suit appending to the deck
+	for i := 0; i < len(types); i++ {
+		for n := 0; n < len(suits); n++ {
+			card := Card{
+				Type: types[i],
+				Suit: suits[n],
+			}
+			deck = append(deck, card)
+		}
+	}
+	return
+}
+
+// Shuffle the deck
+func Shuffle(d Deck) Deck {
+	for i := 1; i < len(d); i++ {
+		// Create a random int up to the number of cards
+		r := rand.Intn(i + 1)
+
+		// If the the current card doesn't match the random
+		// int we generated then we'll switch them out
+		if i != r {
+			d[r], d[i] = d[i], d[r]
+		}
+	}
+	return d
+}
+
+// Deal a specified amount of cards
+func DealCard(d Deck, n int) (Card, Deck) {
+	c, d := d[len(d)-1], d[:len(d)-1]
+	return c, d
+
+}
+
+func EvaluateHand(cards []Card) uint32 {
+	toEval := []string{}
+	for _, c := range cards {
+		toEval = append(toEval, c.String())
+	}
+	return Logic(toEval)
+
 }
--- poker/deck/evaluate_test.go
+++ poker/deck/evaluate_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestServer_CreatePlayer(t *testing.T) {
+func TestDeck_Logic(t *testing.T) {
 
 	tests := []struct {
 		Name     string
@@ -39,6 +39,55 @@ func TestServer_CreatePlayer(t *testing.T) {
 		t.Run(tt.Name, func(t *testing.T) {
 			score := deck.Logic(tt.hand)
 			assert.Equal(t, int(tt.ExpScore), int(score))
+
+		})
+
+	}
+
+}
+
+func TestDeck_Evaluate(t *testing.T) {
+
+	tests := []struct {
+		Name     string
+		hand     []string
+		ExpScore uint32
+	}{
+		{
+			Name:     "Parses a hand",
+			hand:     []string{"Ah", "Ad", "4s", "Ac", "As"},
+			ExpScore: 20,
+		},
+		{
+			Name:     "Best hand possible",
+			hand:     []string{"As", "Ks", "Qs", "Js", "Ts"},
+			ExpScore: 1,
+		},
+		{
+			Name:     "Worst hand possible",
+			hand:     []string{"7h", "5d", "4c", "3s", "2h"},
+			ExpScore: 7462,
+		},
+		{
+			Name:     "Worst hand possible",
+			hand:     []string{"Ts", "9d", "8c", "7c", "6hh"},
+			ExpScore: 1604,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			hand := []deck.Card{}
+			for _, c := range tt.hand {
+				r := string(c[0])
+				s := string(c[1])
+				c := deck.Card{
+					Type: r,
+					Suit: s,
+				}
+				hand = append(hand, c)
+			}
+			assert.Equal(t, int(tt.ExpScore), int(deck.EvaluateHand(hand)))
 
 		})
 
